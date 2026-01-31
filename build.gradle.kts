0496448c6d9c8b036fcc6fbe390ae6aac5b38ab1
Natural-Minecraft <naturalsmp.mc@gmail.com>
2026-01-31 14:23:37 +0700

    ci: Fix shadowJar execution and artifact upload in workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    id("com.github.johnrengelman.shadow").version("8.1.1")
+    id("com.github.johnrengelman.shadow") version "8.1.1"
     `maven-publish`
 }
 
@@ -8,33 +8,29 @@ group = "id.naturalsmp"
 version = "2.12.10"
 
 repositories {
-  mavenCentral()
-
-  maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots") }
-  maven { url = uri("https://hub.spigotmc.org/nexus/content/repositories/snapshots/") }
-  maven { url = uri("https://repo.extendedclip.com/content/repositories/placeholderapi/") }
-  maven { url = uri("https://maven.enginehub.org/repo/") }
-  maven { url = uri("https://gitlab.com/api/v4/projects/19978391/packages/maven") }
+    mavenCentral()
+    maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots") }
+    maven { url = uri("https://hub.spigotmc.org/nexus/content/repositories/snapshots/") }
+    maven { url = uri("https://repo.extendedclip.com/content/repositories/placeholderapi/") }
+    maven { url = uri("https://maven.enginehub.org/repo/") }
+    maven { url = uri("https://gitlab.com/api/v4/projects/19978391/packages/maven") }
 }
 
 dependencies {
-  compileOnly("org.spigotmc:spigot-api:1.21.3-R0.1-SNAPSHOT") // Updated to newer API
-  compileOnly("me.clip:placeholderapi:2.11.6")
-  compileOnly("com.sk89q.worldedit:worldedit-bukkit:7.0.1")
-  compileOnly(files("libs/InfiniteJump.jar"))
+    compileOnly("org.spigotmc:spigot-api:1.21.3-R0.1-SNAPSHOT")
+    compileOnly("me.clip:placeholderapi:2.11.6")
+    compileOnly("com.sk89q.worldedit:worldedit-bukkit:7.0.1")
+    compileOnly(files("libs/InfiniteJump.jar"))
 
-  implementation("com.zaxxer:HikariCP:3.4.5")
-  implementation("org.slf4j:slf4j-simple:1.6.4")
-  implementation("us.ajg0702:ajUtils:1.0.0")
+    implementation("com.zaxxer:HikariCP:3.4.5")
+    implementation("org.slf4j:slf4j-simple:1.6.4")
+    implementation("us.ajg0702:ajUtils:1.0.0")
 }
 
 tasks.withType<ProcessResources> {
-    include("**/*.yml")
-    filter<org.apache.tools.ant.filters.ReplaceTokens>(
-            "tokens" to mapOf(
-                    "VERSION" to project.version.toString()
-            )
-    )
+    filesMatching("plugin.yml") {
+        expand("VERSION" to project.version)
+    }
 }
 
 tasks.shadowJar {
@@ -43,6 +39,10 @@ tasks.shadowJar {
 
     archiveClassifier.set("")
     archiveFileName.set("NaturalParkour.jar")
+}
+
+tasks.build {
+    dependsOn(tasks.shadowJar)
 }
 
 java {
@@ -57,14 +57,11 @@ publishing {
     }
 
     repositories {
-
         val mavenUrl = "https://repo.ajg0702.us/releases"
-
-        if(!System.getenv("REPO_TOKEN").isNullOrEmpty()) {
+        if (!System.getenv("REPO_TOKEN").isNullOrEmpty()) {
             maven {
                 url = uri(mavenUrl)
                 name = "ajRepo"
-
                 credentials {
                     username = "plugins"
                     password = System.getenv("REPO_TOKEN")
