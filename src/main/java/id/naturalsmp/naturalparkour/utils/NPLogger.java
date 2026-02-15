6a3466b2f0cce60354f56c93ddf45dff3097cc81
Natural-Minecraft <naturalsmp.mc@gmail.com>
2026-02-15 10:21:44 +0700

    Fix NPLogger color stripping & console output

--- src/main/java/id/naturalsmp/naturalparkour/utils/NPLogger.java
+++ src/main/java/id/naturalsmp/naturalparkour/utils/NPLogger.java
@@ -2,19 +2,21 @@ package id.naturalsmp.naturalparkour.utils;
 
 import id.naturalsmp.naturalparkour.Main;
 import org.bukkit.Bukkit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class NPLogger {
 
     public static void info(String message) {
-        Main.getInstance().getLogger().info(ChatUtils.colorize(message));
+        Main.getInstance().getLogger().info(stripColor(ChatUtils.colorize(message)));
     }
 
     public static void warn(String message) {
-        Main.getInstance().getLogger().warning(ChatUtils.colorize(message));
+        Main.getInstance().getLogger().warning(stripColor(ChatUtils.colorize(message)));
     }
 
     public static void error(String message) {
-        Main.getInstance().getLogger().severe(ChatUtils.colorize(message));
+        Main.getInstance().getLogger().severe(stripColor(ChatUtils.colorize(message)));
     }
 
     public static void debug(String message) {
@@ -25,5 +27,10 @@ public class NPLogger {
 
     public static void log(String message) {
         Bukkit.getConsoleSender().sendMessage(ChatUtils.colorize(message));
+    }
+
+    private static String stripColor(String message) {
+        // Simple strip for legacy logger to avoid weird characters
+        return org.bukkit.ChatColor.stripColor(message);
     }
 }
